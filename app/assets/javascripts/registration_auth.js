b8e6d31040d54e87482499d826b544ac6aa732cc
roryg11 <Rory.C.Grant@gmail.com>
2015-02-04 11:35:22 -0800

    javascript for sign-up working

--- app/assets/javascripts/registration_auth.js
+++ app/assets/javascripts/registration_auth.js
@@ -1,40 +1,37 @@
 // Sign-Up
 
 $(document).ready(function(){
-  $('.submit').on('click', function(){
-  var userName = $('#new-user-name').val();
-  var userEmail = $('#new-email').val();
-  var userAge = $('#new-age').val();
-  var userBio = $('#new-bio').val();
-  var userZipcode = $('#new-zipcode').val();
-  var userPassword = $('input[type="password"]').val();
-  var userPasswordConfirmation = $('input[type="password"]').val();
+  $('form').submit(function(event){
+    event.preventDefault();
+    var formData = new FormData(this);
+    console.log(formData);
     $.ajax({
       url: '/sign-up',
       type: 'post',
-      data: {
-        user: {
-          name: userName,
-          email: userEmail,
-          age: userAge,
-          bio: userBio,
-          zipcode: userZipcode,
-          password: userPassword,
-          passwordConfirmation: userPasswordConfirmation
-        }
-      }
+      data: formData,
+      cache: false,
+      contentType: false,
+      processData: false
     }).done(function(){
-      console.log("AFTER AJAX");
-      // generate form for picture
-      $('#new-form').empty();
-      var el = '<h4>Upload a Profile Picture</h4><div class = "field"><%= f.file_field :avatar %></div><%= f.submit "Submit" %><% end %>'
-      $('#new-form').append(el);
-    }).fail(function(){
-      console.log("It didn't go through");
+      console.log("User saved");
     });
-  })
+  });
 
   $('#sign-in-submit').on('click', function(){
     var userEmail = $('#email-sign-in').val();
+    var userPassword = $('#password-input').val();
+    console.log(userEmail);
+    console.log(userPassword);
+    $.ajax({
+      url: '/sign-in',
+      data: {
+        user: {
+          email: userEmail,
+          password: userPassword,
+        }
+      }
+    }).done(function(data){
+      console.log("SUCCESS");
+    })
   });
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    if @user.save
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
   end
 
   def edit
