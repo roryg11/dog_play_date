8f5b02e2846cc9e56e98261fb413eb66d2efd645
roryg11 <Rory.C.Grant@gmail.com>
2015-02-03 19:40:33 -0800

    styles and scripts edits before branch change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,36 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(
+    :name,
+    :email,
+    :password,
+    :password_confirmation,
+    :age,
+    :zipcode,
+    :gender,
+    :bio,
+    :avatar
+    )
+  end
 
 end
