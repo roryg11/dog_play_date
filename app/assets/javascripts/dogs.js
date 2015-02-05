73571efba7f7065dd69a979fa7e81cb951f0c9e0
roryg11 <Rory.C.Grant@gmail.com>
2015-02-05 12:54:30 -0800

    working modal for dogs

--- app/assets/javascripts/dogs.js
+++ app/assets/javascripts/dogs.js
@@ -11,4 +11,36 @@ $(document).ready(function(){
       $(this).text('less');
       };
   });
+
+  $('.card').on('click', 'div.header', function(){
+    console.log("YOU CLICKED A DOG!");
+    var dogId = $(this).data('dog-id');
+    var imagePath = $(this).find('img').attr('src');
+    console.log(imagePath);
+    var url = '/dogs/' + dogId
+    $.ajax(url, {type: 'get'}).done(function(data){
+
+      var dogModal = '<div class="ui modal">\
+      <i class="close icon"></i>\
+      <div class="header">' +data["name"]+'</div>\
+      <div class="content">\
+      <div class="ui medium circular image">\
+      <img src ="'+imagePath+'">\
+      </div>\
+      <div class="description"><h3>A Little Bit About Me</h3>\
+      <p>'+data["bio"]+'</p>\
+      <ul><li>Breed: '+data["breed"]+'</li><li>Gender: '+data["gender"]+'</li>\
+      <li>Play Type: '+data["play"]+'</li><li>Personality Type: '+data["personality"]+'</li></ul>\
+      </div>\
+      </div>\
+      <div class="actions">\
+      <div class="ui button">OK</div>\
+      </div>\
+      </div>'
+;
+      $('body').append(dogModal);
+      $('.ui.modal')
+      .modal('show')
+    });
+  })
 });
