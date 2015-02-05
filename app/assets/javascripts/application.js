eaca995a8b9d1d821558f9a10909c1f2d1787dba
roryg11 <Rory.C.Grant@gmail.com>
2015-02-04 17:32:12 -0800

    working multiple page sign-in/sign-up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,4 @@
 //= require_tree .
 // Loads all Semantic javascripts
 //= require semantic-ui
+//= require handlebars-v2.0.0.js
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,28 +1,30 @@
 class RegistrationsController < ApplicationController
-  # def new
-  #   @user = User.new
-  # end
+  def new
+    @user = User.new
+  end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] =@user.id
-      render json: @user
+      session[:user_id] = @user.id
+      redirect_to root_path
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render :new
     end
   end
 
   private
   def user_params
-    params.require(:user).permit(:name,
-                                :email,
-                                :password,
-                                :password_confirmation,
-                                :age,
-                                :zipcode,
-                                :gender,
-                                :bio,
-                                :avatar)
+    params.require(:user).permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation,
+      :age,
+      :zipcode,
+      :gender,
+      :bio,
+      :avatar
+    )
   end
 end
