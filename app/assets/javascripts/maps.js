ede4abbd930c3a52fce2e729631d251bd92af036
roryg11 <Rory.C.Grant@gmail.com>
2015-02-05 21:36:52 -0800

    working maps API feature

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -5,6 +5,7 @@ var newLatLng;
 var newLat;
 var newLng;
 var latlng;
+var mapElement;
 
 function initialize() {
   geocoder = new google.maps.Geocoder();
@@ -12,7 +13,6 @@ function initialize() {
   zipcode = userBlock.attributes[1].value;
   geocoder.geocode({'address': zipcode}, function(results, status){
     if(status == google.maps.GeocoderStatus.OK) {
-      console.log("before map render")
       newLatLng = results[0].geometry.location;
       newLat = (newLatLng["k"]);
       newLng = (newLatLng["D"]);
@@ -24,39 +24,50 @@ function initialize() {
       map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
       var marker = new google.maps.Marker({
         map: map,
-        position: results[0].geometry.location
+        position: results[0].geometry.location,
       });
 
       var request = {
         location: latlng,
-        radius: '2000',
-        types: ['park']
+        radius: '1000',
+        types: ['park'],
+        rankBy: google.maps.places.RankBy.PROMINENCE
       }
-      console.log("before places search");
       var service =  new google.maps.places.PlacesService(map);
       service.nearbySearch(request, callback);
     } else {
       console.log("Geocode was not successful for the following reason: " + status);
     };
 
-    console.log("Before end of geocode call");
   });
-
-  console.log("After geocode call");
 }
 
 function callback(results, status){
-  console.log(results);
+  var mapDiv = document.getElementById('map-segment');
   if(status == google.maps.places.PlacesServiceStatus.OK){
     for(var i= 0; i< results.length; i++){
-      console.log(results[i]);
       var place = results[i];
       createMarker(results[i]);
+      var itemDiv = document.createElement('div');
+      itemDiv.className = "item";
+      mapDiv.appendChild(itemDiv);;
+      var mapIcon = document.createElement('i');
+      mapIcon.className = "map marker icon";
+      itemDiv.appendChild(mapIcon);
+      var contentDiv = document.createElement('div');
+      contentDiv.className = "content";
+      itemDiv.appendChild(contentDiv);
+      var placeHeader = document.createElement('a');
+      placeHeader.className = "header";
+      placeHeader.innerText = results[i]["name"];
+      contentDiv.appendChild(placeHeader);
     }
   }
 }
 
+
 function createMarker(results){
+  console.log(results);
   var marker = new google.maps.Marker({
     map: map,
     position: results.geometry.location
