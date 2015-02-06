f2a6b18736f179e6e49e50ec1f48932fc3dd0cc2
Dana Mulder <danarmulder@gmail.com>
2015-02-05 20:36:56 -0800

    messages only display 10 with a show more

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,6 +6,14 @@ class MessagesController < ApplicationController
 
   def index
     @messages = @conversation.messages
+    if @messages.length > 10
+      @over_ten = true
+      @messages = @messages[-10..-1]
+    end
+    if params[:m]
+      @over_ten = false
+      @messages = @conversation.messages
+    end
   end
 
   def new
