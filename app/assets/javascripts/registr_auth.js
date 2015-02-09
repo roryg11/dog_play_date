94c0256871044a830a1699b7a9a6b08d485e30f0
roryg11 <Rory.C.Grant@gmail.com>
2015-02-09 13:20:18 -0800

    javascript validations using semantic UI

---
+++ app/assets/javascripts/registr_auth.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  $('.ui.form')
+  .form({
+    name: {
+      identifier  : 'user[name]',
+      rules: [
+      {
+        type   : 'empty',
+        prompt : 'Please enter your name'
+      }
+      ]
+    },
+    gender: {
+      identifier  : 'user[gender]',
+      rules: [
+      {
+        type   : 'empty',
+        prompt : 'Please select a gender'
+      }
+      ]
+    },
+
+    password: {
+      identifier : 'user[password]',
+      rules: [
+      {
+        type   : 'empty',
+        prompt : 'Please enter a password'
+      },
+      {
+        type   : 'length[6]',
+        prompt : 'Your password must be at least 6 characters'
+      }
+      ]
+    }
+  }, {inline: true});
+  ;
+})
