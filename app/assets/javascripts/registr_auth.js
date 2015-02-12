de9c4eb1c89d0545fc896a297dc2864eb9523cbf
roryg11 <Rory.C.Grant@gmail.com>
2015-02-11 18:41:03 -0800

    javascript validations for registration and new dog

--- app/assets/javascripts/registr_auth.js
+++ app/assets/javascripts/registr_auth.js
@@ -1,38 +1,108 @@
-// $(document).ready(function(){
-//   $('.ui.form')
-//   .form({
-//     name: {
-//       identifier  : 'user[name]',
-//       rules: [
-//       {
-//         type   : 'empty',
-//         prompt : 'Please enter your name'
-//       }
-//       ]
-//     },
-//     gender: {
-//       identifier  : 'user[gender]',
-//       rules: [
-//       {
-//         type   : 'empty',
-//         prompt : 'Please select a gender'
-//       }
-//       ]
-//     },
-//
-//     password: {
-//       identifier : 'user[password]',
-//       rules: [
-//       {
-//         type   : 'empty',
-//         prompt : 'Please enter a password'
-//       },
-//       {
-//         type   : 'length[6]',
-//         prompt : 'Your password must be at least 6 characters'
-//       }
-//       ]
-//     }
-//   }, {inline: true});
-//   ;
-// })
+$(document).ready(function(){
+  $('#signup-form')
+  .form({
+    name: {
+      identifier  : 'user[name]',
+      rules: [
+      {
+        type   : 'empty',
+        prompt : 'Please enter your name'
+      },
+      {
+        type   : 'length[2]',
+        prompt : 'name must be longer than two characters'
+      }
+      ]
+    },
+    email: {
+      identifier  : 'user[email]',
+      rules: [
+      {
+        type   : 'email',
+        prompt : 'Please enter a valid email'
+      }
+      ]
+    },
+    zipcode: {
+      identifier  : 'user[zipcode]',
+      rules: [
+      {
+        type   : 'integer',
+        prompt : 'Please enter a zipcode'
+      }
+      ]
+    },
+    gender: {
+      identifier  : 'user[gender]',
+      rules: [
+      {
+        type   : 'empty',
+        prompt : 'Please select a gender'
+      }
+      ]
+    },
+
+    age: {
+      identifier  : 'user[age]',
+      rules: [
+      {
+        type   : 'integer',
+        prompt : 'Please enter your age'
+      }
+      ]
+    },
+
+    password: {
+      identifier : 'user[password]',
+      rules: [
+      {
+        type   : 'empty',
+        prompt : 'Please enter a password'
+      },
+      {
+        type   : 'length[6]',
+        prompt : 'Your password must be at least 6 characters'
+      }
+      ]
+    },
+    password_confirmation: {
+      identifier : 'user[password_confirmation]',
+      rules: [
+      {
+        type   : 'match[user[password]]',
+        prompt : 'Passwords do not match'
+      }
+      ]
+    }
+  }, {inline: true});
+  $('#new_dog')
+  .form({
+    name: {
+      identifier  : 'dog[name]',
+      rules: [
+      {
+        type   : 'empty',
+        prompt : 'Please enter your name'
+      }
+      ]
+    },
+    breed: {
+      identifier  : 'dog[breed]',
+      rules: [
+      {
+        type   : 'empty',
+        prompt : "If you don't know you can always put mutt"
+      }
+      ]
+    },
+    gender: {
+      identifier  : 'dog[gender]',
+      rules: [
+      {
+        type   : 'empty',
+        prompt : 'Please let us know what gender your dog is'
+      }
+      ]
+    } 
+  }, {inline: true});
+})
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,6 +1,6 @@
 class Dog < ActiveRecord::Base
   belongs_to :user
-  validates :name, :age, :gender, :size, :play, presence: true
+  validates :name, :breed, :gender, presence: true
 
   has_attached_file :image, :default_url => "/images/normal/missingdog.jpg"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
